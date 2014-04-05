21b729a0f4ddd7ce22b8f3b9eff94b5b7291b347
Kirill Yakovenko <kirill.yakovenko@me.com>
2014-04-05 15:46:52 +0300

    Alias ga for git add

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gul='git pull --prune'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias ga='git add'
 alias gd='git diff'
 alias gc='git commit -m'
 alias gca='git commit -am'
